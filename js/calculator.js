628256c37eefc6da3c2fcd1578d252896e7fa050
lidankatzav <lidankatzav5@gmail.com>
2022-11-19 16:32:05 +0200

    update 2 19.11

--- js/calculator.js
+++ js/calculator.js
@@ -20,7 +20,7 @@ function eval(operand_1, operator, operand_2) {
       current_val = operand_1 / operand_2;
       break;
     default:
-      current_val = undefined;
+      current_val = operand_1;
       break;
   }
   return current_val;
@@ -37,15 +37,57 @@ function active_button(value) {
     }
   } else {
     if (value === "=") {
-      result = eval(parseInt(operand_1), operator, parseInt(operand_2));
+      result = eval(parseFloat(operand_1), operator, parseFloat(operand_2));
+      operand_1 = result;
+      operand_2 = "";
+      operator = "";
     } else if (value === "C") {
       operand_1 = "";
       operand_2 = "";
       operator = "";
       result = "";
+    } else if (value === "erase") {
+      if (operand_2 === "" && operator === "") {
+        operand_1 = operand_1.slice(0, -1);
+        result = operand_1;
+      } else if (operand_2 === "") {
+        operator = "";
+        result = operand_1;
+      } else {
+        operand_2 = operand_2.slice(0, -1);
+        result = operand_1 + " " + operator + " " + operand_2;
+      }
+    } else if (value === ".") {
+      if (operand_2 === "") {
+        operand_1 += ".";
+        result = operand_1;
+      } else {
+        operand_2 += ".";
+        result = operand_1 + " " + operator + " " + operand_2;
+      }
+    } else if (value === "pm") {
+      if (operand_2 === "") {
+        if (operand_1[0] === "-") {
+          operand_1 = operand_1.slice(1);
+        } else {
+          operand_1 = "-" + operand_1;
+        }
+        result = operand_1;
+      } else {
+        if (operand_2[0] === "-") {
+          operand_2 = operand_2.slice(1);
+        } else {
+          operand_2 = "-" + operand_2;
+        }
+        result = operand_1 + " " + operator + " " + operand_2;
+      }
     } else {
       if (operand_1 !== "" && operand_2 !== "") {
-        operand_1 = eval(parseInt(operand_1), operator, parseInt(operand_2));
+        operand_1 = eval(
+          parseFloat(operand_1),
+          operator,
+          parseFloat(operand_2)
+        );
         operand_2 = "";
       }
       operator = value;
@@ -56,9 +98,19 @@ function active_button(value) {
 }
 
 function add_listeners() {
-  const lst = document.querySelectorAll(".digits, .operators, #eval, #reset");
-  for (let i = 0; i < lst.length; i++) {
-    lst[i].addEventListener("click", () => active_button(lst[i].textContent));
+  const lst_by_text = document.querySelectorAll(
+    ".digits, .operators, #eval, #reset, #point"
+  );
+  for (let i = 0; i < lst_by_text.length; i++) {
+    lst_by_text[i].addEventListener("click", () =>
+      active_button(lst_by_text[i].textContent)
+    );
+  }
+  const lst_by_value = document.querySelectorAll("#erase, #pm");
+  for (let i = 0; i < lst_by_value.length; i++) {
+    lst_by_value[i].addEventListener("click", () =>
+      active_button(lst_by_value[i].value)
+    );
   }
 }
 
